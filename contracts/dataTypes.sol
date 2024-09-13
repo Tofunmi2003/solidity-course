e9eb4a4a571acf7b766815d5941ac71740b27fb4
Tofunmi2003 <tohfie2003@gmail.com>
2024-09-12 18:50:26 -0800

    Data types in solidity

---
+++ contracts/dataTypes.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+//Solidity data types
+//bool
+contract DataTypes{
+    //bool public hey;
+    //bool public no = true;
+
+// uINT stands for unsigned integer
+// using a lower uint also saves gas fee that is you wont get charged much.
+   // uint8 // ranges from 0-2**8-1//255
+   //  uint16 // ranges from 0-2**16-1//63535
+  // it is always better you go with uint256   
+     // uint256 // ranges from 0-2**256-1//1.1579209e+77
+
+//NOW ADD MIN AND MAX INT
+// this how you get min and max number in solidity
+    //int public minInt= type(int).min;
+    //int public maxInt = type(int).max;
+
+
+//ARRAY
+
+//----ficed size bytes arrays
+//-----dynamically sized arrays
+     // bytes1 public a = 0xb5;
+     // bytes1 public b = 0xc4;
+
+//DDRESS TYPES
+   //address public adebonmire;
+  // address public addr= 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+
+
+//DEFAULT VALUES
+
+     //bool public defaultBool;// false
+    // uint public number;// 0
+    // int public defaultInt; // 0
+    // address public hey;//0x000000000
+
+
+
+
+
+
+
+
+}
