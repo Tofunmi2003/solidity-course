b0d2e13dbee55d9cfd0e843038db643406010d4c
Tofunmi2003 <tohfie2003@gmail.com>
2024-09-17 15:58:52 -0800

    Error Handlers in Solidity

--- contracts/Require.sol
+++ contracts/Require.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.14;
 // Solidity error handling using the "REQUIRE KEYWORD"
 //REQUIRE KEYWORD IN SOLIDITY
-
-contract Require{
+// the require statement syntex carrys require(statement, message you want to display)
+/*contract Require{
     function checkInput(uint256 _input) public view returns(string memory){
        require(_input >= 0, "invalid uint8");
        require(_input <= 255, "invalid uint8");
@@ -14,5 +14,44 @@ contract Require{
     require(_input % 2 != 0);
     return true;
  }
+
+}
+*/
+//ASSERT ERROR HANDLER
+
+/*contract AssertStatement{
+ 
+     bool public result;
+
+     function checkOverFlow(uint256 _num1, uint256 _num2)public{
+       uint256 sum = _num1+_num2;
+       assert(sum <=225);
+       result = true;
+     }
+
+     function getResult() public view returns(string memory){
+        if (result == true){
+          return "No Over";
+        }else {
+          return "Overflow exist";
+        }
+
+     }
+
+
+}*/
+
+//REVERT ERROR HANDLER
+
+contract RevertStatement{
+       function checkOverflow(uint256 _num1, uint256 _num2)public view returns(string memory){
+
+          uint256 sum = _num1+_num2;
+          if(sum < 0 || sum > 255){
+            revert("overflow exist");
+          }else{
+            return ("No Overflow");
+          }
+       }
 
 }
